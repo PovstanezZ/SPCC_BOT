304e6cadf1354ef643028f16767ff8dfa20a9b5f
PovstanezZ <kontr.tero@gmail.com>
2024-12-03 12:29:57 +0300

    Davai po novoi...

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import telebot
-from telegramBot.config import API
-# from telegramBot.body import *
-from telegramBot.registration import *
-from telegramBot.data_base_code import *
-# from telegramBot.Selection_of_components import *
+from telegramBot.config import *
+from telegramBot.body import *
 
 bot.infinity_polling()
